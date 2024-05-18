3ed9748772e689b4d1b755584fc044a07e676d67
orbu13 <orelbutbul13@gmail.com>
2024-05-18 17:37:18 -0400

    finalProject

--- scripts.js
+++ scripts.js
@@ -19,11 +19,13 @@ function showSuccess(input){
 }
 
 //Check if email is valid
-function isValidEmail(email){
+function checkEmail(input){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return re.test(String(email).toLocaleLowerCase())
-
-    
+    if(re.test(input.value.trim())){
+        showSuccess(input)
+    }else{
+        showError(input, `Email is not valid`)
+    }
 }
 
 //Check required field
@@ -37,6 +39,24 @@ function checkRequired(inputArr){
     })
 }
 
+//Check input length
+function checkLength(input, min, max){
+    if(input.value.length < min){
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    }else if(input.value.length > max){
+        showError(input, `${getFieldName(input)} must be less then ${max} characters`)
+    }else{
+        showSuccess(input)
+    }
+}
+
+//Check password match
+function checkPasswordMatch(input1, input2){
+    if(input1.value !== input2.value){
+        showError(input2, 'Passwords do not match')
+    }
+
+}
 //Get Filed Name
 function getFieldName(input){
     return input.id.charAt(0).toUpperCase() + input.id.slice(1)
@@ -46,10 +66,11 @@ function getFieldName(input){
 form.addEventListener("submit", function(e){
     e.preventDefault()
 
-
     checkRequired([userName, email, password, password2])
-
-
+    checkLength(userName, 3, 15)
+    checkLength(password, 6, 20)
+    checkEmail(email)
+    checkPasswordMatch(password, password2)
 
 })
 // if(userName.value === ''){
